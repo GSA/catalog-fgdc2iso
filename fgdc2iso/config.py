1763281a5376bcbecec839d7c114601708871876
jbrown-xentity <jbrown@xentity.com>
2021-02-17 13:30:34 -0700

    Search for xslt in local directory via dif means

--- fgdc2iso/config.py
+++ fgdc2iso/config.py
@@ -4,5 +4,9 @@ print(os.getcwd())
 
 default_xslt = os.path.join(
     os.path.dirname(os.path.abspath(__file__)),
-    # os.getcwd(),
     'fgdcrse2iso19115-2.xslt')
+
+if not os.path.isFile(default_xslt):
+    default_xslt = os.path.join(
+        os.getcwd(),
+        'fgdcrse2iso19115-2.xslt')
