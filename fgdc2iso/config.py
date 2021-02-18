208a80d07905e7f0dcbea6d0542e6401fab6aa31
Preston Sharpe <pjsharpe07@gmail.com>
2021-02-17 20:28:41 -0700

    fix local build in config.py

--- fgdc2iso/config.py
+++ fgdc2iso/config.py
@@ -1,12 +1,12 @@
 import os
 
+# this should work for local build
 default_xslt = os.path.join(
     os.path.dirname(os.path.abspath(__file__)),
     'fgdcrse2iso19115-2.xslt')
-try:
-    if not os.path.isFile(default_xslt):
-        default_xslt = os.path.join(
-            os.getcwd(),
-            'fgdcrse2iso19115-2.xslt')
-except Exception as e:
-    print('There is a problem with this code: ')
+
+# this should be the path for cloud.gov
+if not os.path.isfile(default_xslt):
+    default_xslt = os.path.join(
+        os.getcwd(),
+        'fgdcrse2iso19115-2.xslt')
